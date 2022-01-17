499c01b2d545609f7a4e2eabdd059be105cb6533
Khalil <khalilnasri92@gmail.com>
2022-01-18 00:47:50 +0100

    required changes

--- src/components/booksComponent/BooksItem.js
+++ src/components/booksComponent/BooksItem.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const BooksItem = () => (
   <div>
-    <h2>Add New Book</h2>
-    <form className="form">
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Categorie" />
-      <button type="submit">Add Book</button>
-    </form>
+    <h2>Mein Kampf</h2>
+    <p>Adolf Hitler</p>
+    <button type="button">Delete</button>
   </div>
 );
 
